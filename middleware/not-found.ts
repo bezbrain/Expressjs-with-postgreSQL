7c4f99d73e9958441e58a0a2ed46285dd9d92074
bezbrain <belloquadriolawale@gmail.com>
2024-03-23 10:19:12 +0100

    Convert the notfound middleware to typesript

--- middleware/not-found.ts
+++ middleware/not-found.ts
@@ -1,4 +1,10 @@
-const NotFoundMiddleware = async (req, res, next) => {
+import { Request, Response, NextFunction } from "express";
+
+const NotFoundMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   return res.status(404).send("Route does not exist");
 };
 
