21caecad0592a00af6329d848bdaf3cd965f75f3
bezbrain <belloquadriolawale@gmail.com>
2024-04-05 14:22:17 +0100

    Sign a user using jwt

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,19 +4,33 @@ import { hashPass } from "../schemaValidation/hashPassword";
 import { createUser } from "../repository/user.repo";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError } from "../errors";
+import { v4 } from "uuid";
+import { signUser } from "../schemaValidation/jwtSign";
 
 const register = async (req: Request, res: Response) => {
-  const { name, email, username, password } = req.body;
+  const { id, name, email, username, password } = req.body;
+
+  //   Extract userId using rest operator
+  const { userId, ...userData } = req.body;
+  //   Extract userPassword using rest operator
+  const { userPassword, ...jwtData } = req.body;
 
   //   Validate user inputs
-  const user = await UserSchema.validateAsync(req.body);
+  const user = await UserSchema.validateAsync(userData);
 
   //   Hash the password
   const hashPassword = await hashPass(user.password);
 
-  const userCreate = await createUser(name, email, username, hashPassword);
+  const userCreate = await createUser(
+    v4(),
+    name,
+    email,
+    username,
+    hashPassword
+  );
 
-  console.log(userCreate);
+  //   Invoke sign a user if user registration successful
+  const token = await signUser(jwtData);
 
   //   Check if user was not successfully created
   if (userCreate.rowCount === 0) {
@@ -31,8 +45,9 @@ const register = async (req: Request, res: Response) => {
       email,
       username,
     },
+    token,
   });
-  // res.send("Register a user");
+  //   res.send("Register a user");
 };
 
 export { register };
--- src/repository/user.repo.ts
+++ src/repository/user.repo.ts
@@ -2,14 +2,15 @@ import db from "../datasource/db";
 import { v4 } from "uuid";
 
 const createUser = async (
+  id: string,
   name: string,
   email: string,
   username: string,
   password: string
 ) => {
   return await db.query(
-    `INSERT INTO users (id, name, email, username, password) VALUES ('${v4()}', $1, $2, $3, $4)`,
-    [name, email, username, password]
+    `INSERT INTO users (id, name, email, username, password) VALUES ($1, $2, $3, $4, $5)`,
+    [id, name, email, username, password]
   );
 };
 
